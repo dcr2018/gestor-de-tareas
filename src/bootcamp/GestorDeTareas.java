4b1106c14de51320873aa45cb209e6c2d1982ed5
Daniel Castañeda Ruiz <daniel.castaneda2019@gmail.com>
2026-02-10 19:19:35 -0300

    Se agrega funcionalidad Eliminar Tarea Se agrega campo prioridad a clase Tarea Se agrega validación de prioridad a funcionalidad Agregar Tarea Se valida que en cada operación sobre las tareas la lista de tareas no esté vacía Se agrega documentación a métodos y clases Se agrega funcionalidad Mostrar tareas completadas Limpieza en el código

--- src/bootcamp/GestorDeTareas.java
+++ src/bootcamp/GestorDeTareas.java
@@ -3,101 +3,178 @@ package bootcamp;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * Clase principal del programa. En esta clase ocurre la gestión de las tareas, incluyendo la presentación del 
+ * menú de opciones al usuario y la ejecución de las acciones correspondientes según la opción seleccionada.
+ */
 public class GestorDeTareas {
 
+    static String opcion;
+    static Scanner scanner;
+
+    /**
+     * Método principal del programa. Realiza la gestión de las tareas. El usuario puede agregar tareas, mostrar 
+     * tareas pendientes, mostrar tareas completas, marcar tareas como completas, eliminar tareas o salir del programa.
+     * @param args Argumentos de línea de comandos (no se utilizan en este programa).
+     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         boolean salir = false;
 
         ArrayList<Tarea> listaDeTareas = new ArrayList<>();
-        listaDeTareas.add(new Tarea("Ir al supermercado", 1));
-        listaDeTareas.add(new Tarea("Lavar el auto", 2));
-        listaDeTareas.add(new Tarea("Pagar las cuentas", 3));
-        listaDeTareas.add(new Tarea("Completar las tareas de la plataforma", 4));
+        listaDeTareas.add(new Tarea("Ir al supermercado", "alta", 1));
+        listaDeTareas.add(new Tarea("Lavar el auto", "media", 2));
+        listaDeTareas.add(new Tarea("Pagar las cuentas", "media", 3));
+        listaDeTareas.add(new Tarea("Completar las tareas de la plataforma","media", 4));
 
         limpiarPantalla();
-        System.out.println("(A)gregar una tarea, (M)ostrar tareas pendientes, (C)ompletar una tarea, (S)alir");
-        String opcion = scanner.nextLine().trim().toLowerCase();
+        mostrarMenu();
 
         while (!salir) {
+            // Agregar tarea
             if (opcion.equals("a")) {
-                // Agregar tarea
-
                 System.out.print("Ingresa la tarea: ");
                 String descripcion = scanner.nextLine().trim();
+                
+                System.out.print("Ingresa la prioridad (alta | media): ");
+                String prioridad = scanner.nextLine().trim().toLowerCase();
+                
+                if (!prioridad.equals("alta") && !prioridad.equals("media")) {
+                    limpiarPantalla();
+                    System.out.println("Prioridad no válida. La prioridad puede ser 'alta' o 'media'.");
+                    mostrarMenu();
+                    continue;
+                }
 
                 Tarea ultimaTarea = listaDeTareas.stream()
                         .max((t1, t2) -> Integer.compare(t1.id, t2.id))
                         .orElse(null);
                 int nuevoId = ultimaTarea != null ? ultimaTarea.id + 1 : 1;
-                                
-                listaDeTareas.add(new Tarea(descripcion, nuevoId));
-                
+
+                listaDeTareas.add(new Tarea(descripcion, prioridad, nuevoId));
+
                 limpiarPantalla();
                 System.out.println("Tarea agregada");
-                System.out
-                        .println("\n(A)gregar una tarea, (M)ostrar tareas pendientes, (C)ompletar una tarea, (S)alir");
-                opcion = scanner.nextLine().trim().toLowerCase();
+                mostrarMenu();
 
-            } else if (opcion.equals("m")) {
-                // Mostrar tareas pendientes
+            }
+            // Mostrar tareas pendientes
+            else if (opcion.equals("p")) {
+                if(listaDeTareas.stream().filter(tarea -> !tarea.completada).count() == 0) {
+                    limpiarPantalla();
+                    System.out.println("No hay tareas pendientes.");
+                    mostrarMenu();
+                    continue;
+                }
 
                 limpiarPantalla();
                 for (Tarea tarea : listaDeTareas) {
                     if (!tarea.completada) {
-                        System.out.println(tarea.id + ") " + tarea.descripcion);
+                        System.out.println(tarea.id + ") " + tarea.descripcion + " [" + tarea.prioridad + "]");
                     }
                 }
+                mostrarMenu();
 
-                System.out
-                        .println("\n(A)gregar una tarea, (M)ostrar tareas pendientes, (C)ompletar una tarea, (S)alir");
-                opcion = scanner.nextLine().trim().toLowerCase();
-
-            } else if (opcion.equals("c")) {
-                // Completar tarea
-
-                System.out.print("Ingrese el número de la tarea a completar: ");
-                try {
-                    int id = Integer.parseInt(scanner.nextLine());
-                    listaDeTareas.stream()
-                            .filter(tarea -> tarea.completada == false)
-                            .filter(tarea -> tarea.id == id)
-                            .findFirst()
-                            .ifPresentOrElse(
-                                    tarea -> {
-                                        tarea.marcarComoCompletada();
-                                        limpiarPantalla();
-                                        System.out.println("La tarea " + id + " ha sido marcada como completada.");
-                                    }, () -> {
-                                        limpiarPantalla();
-                                        System.out.println("No se encontró una tarea con el número " + id + ".");
-                                    });
-
-                    System.out.println(
-                            "\n(A)gregar una tarea, (M)ostrar tareas pendientes, (C)ompletar una tarea, (S)alir");
-                    opcion = scanner.nextLine().trim().toLowerCase();
-                } catch (NumberFormatException e) {
-                    System.out.println("Error: debes ingresar un número.");
-                    System.out.println(
-                            "\n(A)gregar una tarea, (M)ostrar tareas pendientes, (C)ompletar una tarea, (S)alir");
-                    opcion = scanner.nextLine().trim().toLowerCase();
+            } 
+            // Mostrar tareas completas
+            else if (opcion.equals("c")) {
+                if(listaDeTareas.stream().filter(tarea -> tarea.completada).count() == 0) {
+                    limpiarPantalla();
+                    System.out.println("No hay tareas completas aún.");
+                    mostrarMenu();
+                    continue;
                 }
 
-            } else if (opcion.equals("s")) {
-                // Salir
+                limpiarPantalla();
+                for (Tarea tarea : listaDeTareas) {
+                    if (tarea.completada) {
+                        System.out.println(tarea.id + ") " + tarea.descripcion + " [" + tarea.prioridad + "]");
+                    }
+                }
+                mostrarMenu();
 
+            }
+            // Completar tarea
+            else if (opcion.equals("m")) {
+                if(listaDeTareas.stream().filter(tarea -> !tarea.completada).count() == 0) {
+                    limpiarPantalla();
+                    System.out.println("No hay tareas pendientes.");
+                    mostrarMenu();
+                    continue;
+                }
+
+                System.out.print("Ingrese el número de la tarea: ");
+                try {
+                    int id = Integer.parseInt(scanner.nextLine());
+
+                    Tarea tareaAMarcar = listaDeTareas.stream()
+                            .filter(tarea -> !tarea.completada)
+                            .filter(tarea -> tarea.id == id)
+                            .findFirst()
+                            .orElse(null);
+                    
+                    if (tareaAMarcar != null) {
+                        tareaAMarcar.marcarComoCompletada();
+                        limpiarPantalla();
+                        System.out.println("La tarea " + id + " ha sido marcada como completada.");
+                    } else {
+                        limpiarPantalla();
+                        System.out.println("No se encontró una tarea pendiente con el número " + id + ".");
+                    }
+                    mostrarMenu();
+
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: debes ingresar un número.");
+                    mostrarMenu();
+                }
+
+            }
+            // Eliminar tarea
+            else if (opcion.equals("e")) {
+                if(listaDeTareas.isEmpty()) {
+                    limpiarPantalla();
+                    System.out.println("No hay tareas para eliminar.");
+                    mostrarMenu();
+                    continue;
+                }
+
+                System.out.print("Ingrese el número de la tarea a eliminar: ");
+                try {
+                    int id = Integer.parseInt(scanner.nextLine());
+                    boolean tareaEncontrada = listaDeTareas.removeIf(tarea -> tarea.id == id);
+                    limpiarPantalla();
+                    if (tareaEncontrada) {
+                        System.out.println("La tarea " + id + " ha sido eliminada.");
+                    } else {
+                        System.out.println("No se encontró una tarea con el número " + id + ".");
+                    }
+                    mostrarMenu();
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: debes ingresar un número.");
+                    mostrarMenu();
+                }
+            } 
+            // Salir
+            else if (opcion.equals("s")) {
                 salir = true;
-            } else {
-                // Opción no válida
-
+            } 
+            // Opción no válida
+            else {
                 System.out.println("Opción no válida. Intente de nuevo.");
-                System.out.println("(A)gregar una tarea, (M)ostrar tareas pendientes, (C)ompletar una tarea, (S)alir");
-                opcion = scanner.nextLine().trim().toLowerCase();
+                mostrarMenu();
             }
         }
         scanner.close();
     }
 
+    /**
+     * Método para limpiar la pantalla de la consola. 
+     * Para cmd, PowerShell y Windows PowerShell en sus terminales nativas 
+     * usa comandos propios de cada intérprete de comandos para limpiar la pantalla. 
+     * Si ejecuta cmd, PowerShell o Windows PowerShell dentro de Windows Terminal 
+     * usa secuencias ANSI, ya que Windows Terminal las soporta.
+     * Para Git Bash también usa secuencias ANSI.
+     */
     private static void limpiarPantalla() {
         try {
             String wtSession = System.getenv("WT_SESSION"); // Windows Terminal
@@ -125,5 +202,17 @@ public class GestorDeTareas {
         } catch (Exception e) {
             System.out.println("No se pudo limpiar la pantalla.\nError: " + e.getMessage());
         }
+    }
+
+    /**
+     * Presenta al usuario el menú de opciones y lee su elección. Las opciones para gestionar las tareas son:
+     * agregar una tarea, mostrar tareas pendientes, mostrar tareas completas, marcar una tarea como completa, 
+     * eliminar una tarea y salir del programa. 
+     */
+    private static void mostrarMenu() {
+        System.out.println("┌──────────────┬───────────┬──────────────────────┬─────────────────────┬────────────────────────┬────────────┬─────────┐");
+        System.out.println("│  Menú tareas │ (A)gregar │ Mostrar (p)endientes │ Mostrar (c)ompletas │ (M)arcar como completa │ (E)liminar │ (S)alir │"); 
+        System.out.println("└──────────────┴───────────┴──────────────────────┴─────────────────────┴────────────────────────┴────────────┴─────────┘");
+        opcion = scanner.nextLine().trim().toLowerCase();
     }
 }
--- src/bootcamp/Tarea.java
+++ src/bootcamp/Tarea.java
@@ -1,13 +1,25 @@
 package bootcamp;
 
+/**
+ * Representa una tarea en el gestor de tareas.
+ */
 public class Tarea {
 
     public int id;
     public String descripcion;
+    public String prioridad;
     public boolean completada;
 
-    public Tarea(String descripcion, int id) {
+    /**
+     * Construye una nueva tarea con los parámetros especificados.
+     *
+     * @param descripcion la descripción de la tarea
+     * @param prioridad   el nivel de prioridad de la tarea ("alta" o "media")
+     * @param id          el identificador único de la tarea
+     */
+    public Tarea(String descripcion, String prioridad, int id) {
         this.descripcion = descripcion;
+        this.prioridad = prioridad;
         this.id = id;
         this.completada = false;
     }
